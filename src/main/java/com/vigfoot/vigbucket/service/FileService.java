eff7c9d96bf20d4dffab73eda1f2cc6b1658669f
Xasquatch <bigfoot7774@gmail.com>
2023-03-10 12:50:47 +0900

    initialize directory
    Signed-off-by: Xasquatch <bigfoot7774@gmail.com>

--- src/main/java/com/vigfoot/vigbucket/service/FileService.java
+++ src/main/java/com/vigfoot/vigbucket/service/FileService.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Service
 public class FileService {
 
-    @Value("${directory}")
+    @Value("${dir}")
     public String directory;
 
     public List<Bucket.File> getStorage(){
